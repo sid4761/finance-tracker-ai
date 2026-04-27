15f3bc66f434023a1d871532046d3e94231cbbf2
Siddharth Gupta <sgguptafen@gmail.com>
2026-04-28 05:17:40 +0530

    update backend url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+  baseURL: "https://finance-backend-gfd5.onrender.com/api"
 });
 
 export default API;
