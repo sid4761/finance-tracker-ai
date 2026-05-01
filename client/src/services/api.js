dfa21a1c268be92fa71d9dc77cd3449d25718499
Siddharth Gupta <sgguptafen@gmail.com>
2026-05-02 04:05:13 +0530

    fix vercel api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,16 +1,20 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://finance-backend-gfd5.onrender.com/api"
+  baseURL: "https://finance-backend-gfd5.onrender.com/api",
 });
 
-// Automatically attach JWT token
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => Promise.reject(error));
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
